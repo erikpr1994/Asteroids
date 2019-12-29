fa42f1291eb76dce39233a9a827e1226507c9940
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2019-12-29 20:39:53 +0100

    Añadido el funcionamiento de la consola

--- game/game/game.cpp
+++ game/game/game.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "FAriasSimpleGraphics.h"
 
+// Constantes y variables del juego
+const int CONSOLE_WIDTH = 200, CONSOLE_HEIGHT = 50;
 bool gameOver = false;
+
+using namespace FASG;
 
 int main()
 {
+    InitConsole(CONSOLE_WIDTH, CONSOLE_HEIGHT);
+
     while(!gameOver){}
+
+    DestroyConsole();
 }
